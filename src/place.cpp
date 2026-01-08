a85bf577d1a591a527bf628361415a391ae8443e
Simran Pundir <122039675+simranpundirr@users.noreply.github.com>
2026-01-08 23:58:58 +0530

    Implement Place class logic

---
+++ src/place.cpp
@@ -0,0 +1,31 @@
+#include "place.h"
+
+Place::Place(
+    std::string name,
+    std::string fullAddress,
+    std::string city,
+    double latitude,
+    double longitude,
+    std::string category,
+    double rating,
+    int visitTimeHours,
+    int costLevel,
+    std::vector<TravelerType> allowedTypes
+) : name(name),
+    fullAddress(fullAddress),
+    city(city),
+    latitude(latitude),
+    longitude(longitude),
+    category(category),
+    rating(rating),
+    visitTimeHours(visitTimeHours),
+    costLevel(costLevel),
+    allowedTravelerTypes(allowedTypes) {}
+
+bool Place::isSuitableFor(TravelerType type) const {
+    for (auto t : allowedTravelerTypes) {
+        if (t == type)
+            return true;
+    }
+    return false;
+}
